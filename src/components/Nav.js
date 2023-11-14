c99cd3dfb4aab21d1596c79c73865ad8ec8a64d4
bhargavi1098 <bhargavi1098@gmail.com>
2023-11-14 11:06:29 +0530

    second  commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "../images/Logo .svg";
+import logo from "../images/Logo_.svg";
 
 const Nav = () => {
   const [menuOpen, setMenuOpen] = useState(false);
